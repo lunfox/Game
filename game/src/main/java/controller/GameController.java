daaaae49653262f77ad31cbf81d6436b3b928bca
lunfox <dasewhuy@gmail.com>
2022-03-24 22:38:18 +0700

    update Camera (class Cam, layoutCoordinate)

--- game/src/main/java/controller/GameController.java
+++ game/src/main/java/controller/GameController.java
@@ -1,30 +1,51 @@
 package controller;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.text.Text;
-import player.Player;
+import util.Cam;
 
 public class GameController {
 
-    private Player player;
-
-    @FXML
-    private Text stat;
+    private int w = MainController.width / 2;
+    private int h = MainController.height / 2;
+    private double speed = 2;
+    private double dx, dy;
 
     @FXML
     private Pane gameRoot;
 
-    public void startGame(Scene scene) {
-        player = new Player(scene);
-        Circle circle = new Circle(25, Color.BLUE);
-        gameRoot.getChildren().add(circle);
-        scene.setOnMouseMoved(e -> {
-            player.move(1, 1, e, circle);
-         }); 
+    private Cam camera;
 
+    public void startGame(Scene scene) {
+        camera = new Cam(scene);
+        AnimationTimer gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long arg0) {
+                update();
+                render();
+            }
+        };
+        gameLoop.start();
+    }
+
+    public void update() {
+        if(camera.getLastX() > w) {
+            dx = speed;
+        }
+        else {
+            dx = -speed;
+        }
+        if(camera.getLastY() > h) {
+            dy = speed;
+        }
+        else {
+            dy = -speed;
+        }
+    }
+
+    public void render() {
+        camera.move(dx, dy);
     }
 }
--- game/src/main/java/main/Main.java
+++ game/src/main/java/main/Main.java
@@ -2,9 +2,6 @@ package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Camera;
-import javafx.scene.Parent;
-import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
